4e2bb15d9f1f7f72afa788ddd937085b6391a7c4
t4sk <tsk.nakamura@gmail.com>
2025-03-24 16:53:49 +0900

    stake

--- foundry/src/exercises/Stake.sol
+++ foundry/src/exercises/Stake.sol
@@ -14,24 +14,41 @@ contract Stake {
     IRewardRouterV2 constant rewardRouter = IRewardRouterV2(REWARD_ROUTER_V2);
     IRewardTracker constant rewardTracker = IRewardTracker(REWARD_TRACKER);
 
+    // Task 1 - Stake GMX
     function stake(uint256 gmxAmount) external {
-        // Write your code here
+        gmx.transferFrom(msg.sender, address(this), gmxAmount);
+        gmx.approve(REWARD_TRACKER, gmxAmount);
+        rewardRouter.stakeGmx(gmxAmount);
     }
 
+    // Task 2 - Unstake GMX
     function unstake(uint256 gmxAmount) external {
-        // Write your code here
+        rewardRouter.unstakeGmx(gmxAmount);
     }
 
+    // Task 3 - Claim rewards
     function claimRewards() external {
-        // Write your code here
+        gmx.approve(address(rewardTracker), type(uint256).max);
+
+        rewardRouter.handleRewards({
+            shouldClaimGmx: true,
+            shouldStakeGmx: true,
+            shouldClaimEsGmx: false,
+            shouldStakeEsGmx: false,
+            // TODO: wat dis?
+            shouldStakeMultiplierPoints: true,
+            shouldClaimWeth: true,
+            shouldConvertWethToEth: false
+        });
     }
 
+    // Task 4 - Get staked amount
     function getStakedAmount() external view returns (uint256) {
-        // Write your code here
         return rewardTracker.stakedAmounts(address(this));
     }
 
+    // Task 5 - Delegate
     function delegate(address delegatee) external {
-        // Write your code here
+        gmxDao.delegate(delegatee);
     }
 }
--- foundry/src/solutions/Stake.sol
+++ foundry/src/solutions/Stake.sol
@@ -14,16 +14,19 @@ contract Stake {
     IRewardRouterV2 constant rewardRouter = IRewardRouterV2(REWARD_ROUTER_V2);
     IRewardTracker constant rewardTracker = IRewardTracker(REWARD_TRACKER);
 
+    // Task 1 - Stake GMX
     function stake(uint256 gmxAmount) external {
         gmx.transferFrom(msg.sender, address(this), gmxAmount);
         gmx.approve(REWARD_TRACKER, gmxAmount);
         rewardRouter.stakeGmx(gmxAmount);
     }
 
+    // Task 2 - Unstake GMX
     function unstake(uint256 gmxAmount) external {
         rewardRouter.unstakeGmx(gmxAmount);
     }
 
+    // Task 3 - Claim rewards
     function claimRewards() external {
         gmx.approve(address(rewardTracker), type(uint256).max);
 
@@ -39,10 +42,12 @@ contract Stake {
         });
     }
 
+    // Task 4 - Get staked amount
     function getStakedAmount() external view returns (uint256) {
         return rewardTracker.stakedAmounts(address(this));
     }
 
+    // Task 5 - Delegate
     function delegate(address delegatee) external {
         gmxDao.delegate(delegatee);
     }
